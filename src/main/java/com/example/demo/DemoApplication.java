56ff159011f5aba68726185e60542724483c9267
yosra mekaoui <61566287+yosra-mekaoui@users.noreply.github.com>
2022-11-28 23:11:34 +0100

    Scheduler2
    faireEvoluerEquipes()

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ComponentScan(basePackages = {"com.example.demo.Service"})
 @ComponentScan(basePackages = {"com.example.demo.Repository"})
+@EnableScheduling
 @SpringBootApplication
 public class DemoApplication {
 
